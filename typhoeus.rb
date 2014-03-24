b76d1f4eae9be832002ce19b3b6fb13262bc8749
ericnbarrett <ericnbarrett@gmail.com>
2014-03-24 21:01:58 +0400

    updated script

--- typhoeus.rb
+++ typhoeus.rb
@@ -13,38 +13,44 @@ def get_latest_id
   id
 end
 
+def logger(type, status, date=Time.now)
+  file = "logs/"
+  File.open(file, "wa") do |f| 
+    f << "#{type}"
+    f.save
+  end
+end
+
 # get end id
 start_id = 903 
-get_latest_id
+end_id = get_latest_id
 
 
-##initiate hydra
-#hydra = Typhoeus::Hydra.hydra
+#initiate hydra
+hydra = Typhoeus::Hydra.new(max_concurrency: 20)
 
-#request = ''
+request = ''
 
-##build hydra queue
-#(1..id.to_i).map do |i|
-#  puts "Getting: #{url.to_s + i.to_s}"
-#  request = Typhoeus::Request.new("#{url.to_s + i.to_s}", followlocation: true)
-#  hydra.queue(request)
-#end
+#build hydra queue
+(start_id.to_i..end_id.to_i).map do |i|
+  puts "Getting: #{url.to_s + i.to_s}"
+  request = Typhoeus::Request.new("#{url.to_s + i.to_s}", followlocation: true)
+  hydra.queue(request)
+end
 
-#Typhoeus::Hydra.new(max_concurrency: 20)
+request.on_complete do |response|
+  if response.success?
+    # put success callback here
+  elsif response.timed_out?
+    # aw hell no
+    log("got a time out")
+  elsif response.code == 0
+    # Could not get an http response, something's wrong.
+    log(response.return_message)
+  else
+    # Received a non-successful http response.
+    log("HTTP request failed: " + response.code.to_s)
+  end
+end
 
-#request.on_complete do |response|
-#  if response.success?
-#    puts "Yay"
-#  elsif response.timed_out?
-#    # aw hell no
-#    log("got a time out")
-#  elsif response.code == 0
-#    # Could not get an http response, something's wrong.
-#    log(response.return_message)
-#  else
-#    # Received a non-successful http response.
-#    log("HTTP request failed: " + response.code.to_s)
-#  end
-#end
-
-#hydra.run
+hydra.run
